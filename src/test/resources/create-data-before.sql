bb969a7f2f3fd19d1b24f39b601d4e90557d3d02
anton-borovikov <43046637+anton-borovikov@users.noreply.github.com>
2019-03-14 12:52:47 +0300

    Update create-data-before.sql

--- src/test/resources/create-data-before.sql
+++ src/test/resources/create-data-before.sql
@@ -18,8 +18,8 @@ INSERT INTO role (name)
 VALUES ('user');
 
 INSERT INTO usr (username, password, first_name, last_name, age, user_email, activated) VALUES
-  ('user1', crypt('111', gen_salt('bf', 8)), 'Lagutin', 'Vadim', '45', 'jcoder3@gmail.com', TRUE),
-  ('user2', crypt('222', gen_salt('bf', 8)), 'Ivanov', 'Ivan', '31', 'jcoder3@gmail.com', TRUE);
+  ('user1', crypt('111', gen_salt('bf', 8)), 'Lagutin', 'Vadim', '45', '', TRUE),
+  ('user2', crypt('222', gen_salt('bf', 8)), 'Ivanov', 'Ivan', '31', '', TRUE);
 
 INSERT INTO user_role (user_id, role_id) VALUES
   (1, 1),
